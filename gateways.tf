68393678ef90972d7d61a9d328bc444a713bf46f
yishu1606 <yishvendra.baghel@kellton.com>
2025-02-10 13:16:28 +0530

    creating NAT gateway and attaching eip with it, making sure that private subnet doesn't have direct internet access, but internet access through NAT gateway

--- gateways.tf
+++ gateways.tf
@@ -2,3 +2,19 @@ resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.main.id
 }
 
+
+
+resource "aws_eip" "nat" {
+  domain = "vpc"
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public[0].id
+}
+
+resource "aws_route" "private_internet_access" {
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat.id
+}
