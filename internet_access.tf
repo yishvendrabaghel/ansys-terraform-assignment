1a17b6afcd4d18adef4d8d95b12eb6eaef2c6a8f
yishu1606 <yishvendra.baghel@kellton.com>
2025-02-10 13:05:19 +0530

    making sure that public internet has access to internet

---
+++ internet_access.tf
@@ -0,0 +1,15 @@
+
+resource "aws_eip" "nat" {
+  domain = "vpc"
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public[0].id
+}
+
+resource "aws_route" "private_internet_access" {
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat.id
+}
