a863e24979d791c17b22c6f49cfa727a56fd5732
Yishu Baghel <ykbskb@gmail.com>
2025-02-12 16:33:58 +0530

    final file with checkup

--- internet_access.tf
+++ internet_access.tf
@@ -1,11 +1,5 @@
 
 resource "aws_eip" "nat" {
-  domain = "vpc"
-}
-
-resource "aws_nat_gateway" "nat" {
-  allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.public[0].id
 }
 
 resource "aws_route" "private_internet_access" {
